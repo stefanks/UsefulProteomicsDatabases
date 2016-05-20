43437af07f6f493dce6a258d352d7b05cdcbb146
stefanks <solntsev@gmail.com>
2016-05-20 15:27:15 -0500

    For nuget

--- UsefulProteomicsDatabases/Properties/AssemblyInfo.cs
+++ UsefulProteomicsDatabases/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UsefulProteomicsDatabases")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Useful Proteomics Databases")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Stefan Solntsev")]
 [assembly: AssemblyProduct("UsefulProteomicsDatabases")]
